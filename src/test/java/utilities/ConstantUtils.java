11ff8dc798cf22e477058b0e5ff475635b0c02b2
usha <ushadk18@gmail.com>
2024-06-12 21:02:33 +0100

    added utils

---
+++ src/test/java/utilities/ConstantUtils.java
@@ -0,0 +1,18 @@
+package utilities;
+
+public class ConstantUtils {
+
+    //base url of the website:
+    // //https://the-internet.herokuapp.com/
+
+    public static String BASE_URL = "https://the-internet.herokuapp.com/";
+
+    //browser name
+    public static String BROWSER_NAME = "chrome";
+
+    //Browser type (Headed or headless)
+    public static String BROWSER_TYPE = "Headed";
+
+
+
+}
